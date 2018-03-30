2fcc791bf268800b1123ab2df3fa7140440b5b0d
Keloo <vlad-negura@mail.ru>
2018-03-31 02:51:13 +0300

    adding doctrine, jwt and authorization

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class DefaultController extends Controller
 {
@@ -44,5 +48,21 @@ class DefaultController extends Controller
                 'description' => "Plain fields of a type of grass cover most of the fields outside of the castle, adding to the castle's aesthetics. This castle is relatively new, but so far it stood its ground with ease and it'll likely do so for ages to come.",
             ],
         ]);
+    }
+
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $username = $request->request->get('_username');
+        $password = $request->request->get('_password');
+        $user = new User($username);
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $em->persist($user);
+        $em->flush();
+        return new Response(sprintf('User %s successfully created', $user->getUsername()));
+    }
+    public function api()
+    {
+        return new Response(sprintf('Logged in as %s', $this->getUser()->getUsername()));
     }
 }
