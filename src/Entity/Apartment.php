8a867f1531aa6862e72c9cba81d2012282832337
Keloo <vlad-negura@mail.ru>
2018-04-02 18:11:44 +0300

    adding models, User, Apartment

---
+++ src/Entity/Apartment.php
@@ -0,0 +1,157 @@
+<?php declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ApartmentRepository")
+ */
+class Apartment
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $area;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price_per_month;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $room_count;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $gps_latitude;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $gps_longitude;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * Many Apartments have one user.
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="apartments")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $user;
+
+    /**
+     * @return UserInterface
+     */
+    public function getUser(): UserInterface
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param null|UserInterface $user
+     * @return Apartment
+     */
+    public function setUser(?UserInterface $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTime('now');
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getArea(): ?int
+    {
+        return $this->area;
+    }
+
+    public function setArea(int $area): self
+    {
+        $this->area = $area;
+
+        return $this;
+    }
+
+    public function getPricePerMonth(): ?int
+    {
+        return $this->price_per_month;
+    }
+
+    public function setPricePerMonth(int $price_per_month): self
+    {
+        $this->price_per_month = $price_per_month;
+
+        return $this;
+    }
+
+    public function getRoomCount(): ?int
+    {
+        return $this->room_count;
+    }
+
+    public function setRoomCount(int $room_count): self
+    {
+        $this->room_count = $room_count;
+
+        return $this;
+    }
+
+    public function getGpsLatitude(): ?string
+    {
+        return $this->gps_latitude;
+    }
+
+    public function setGpsLatitude(string $gps_latitude): self
+    {
+        $this->gps_latitude = $gps_latitude;
+
+        return $this;
+    }
+
+    public function getGpsLongitude(): ?string
+    {
+        return $this->gps_longitude;
+    }
+
+    public function setGpsLongitude(string $gps_longitude): self
+    {
+        $this->gps_longitude = $gps_longitude;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,69 +2,340 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\Apartment;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
 /**
- * @ORM\Table(name="users")
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User implements UserInterface
+class User implements AdvancedUserInterface, \Serializable
 {
+    const ROLE_DEFAULT = 'ROLE_USER';
+    const ROLE_SUPER_ADMIN = 'ROLE_SUPER_ADMIN';
+
     /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=25, unique=true)
+     * @ORM\Column(type="string", length=255)
      */
     private $username;
 
     /**
-     * @ORM\Column(type="string", length=500)
+     * @ORM\Column(type="string", length=255)
+     */
+    private $usernameCanonical;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $emailCanonical;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $enabled;
+
+    /**
+     * @ORM\Column(type="string", length=255)
      */
     private $password;
 
-    /**
-     * @ORM\Column(name="is_active", type="boolean")
-     */
-    private $isActive;
+    //not persisted (example usage in model validation).
+    private $plainPassword;
 
-    public function __construct($username)
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $confirmationToken;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $roles;
+
+
+    /**
+     * One User has many Apartments.
+     * @ORM\OneToMany(targetEntity="App\Entity\Apartment", mappedBy="user")
+     */
+    private $apartments;
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getApartments(): ArrayCollection
     {
-        $this->isActive = true;
-        $this->username = $username;
+        return $this->apartments;
     }
 
-    public function getUsername()
+    /**
+     * @param \App\Entity\Apartment|null $apartment
+     * @return User
+     */
+    public function addApartment(?Apartment $apartment): self
+    {
+        if ($this->apartments->contains($apartment))
+            return $this;
+
+        $this->apartments[] = $apartment;
+
+        // make the relation bidirectional!
+        $apartment->setUser($this);
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $apartments
+     */
+    public function setApartments($apartments)
+    {
+        $this->apartments = $apartments;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
     {
         return $this->username;
     }
 
-    public function getSalt()
+    public function setUsername(string $username): self
     {
-        return null;
+        $this->username = $username;
+
+        return $this;
     }
 
-    public function getPassword()
+    public function getUsernameCanonical(): ?string
+    {
+        return $this->usernameCanonical;
+    }
+
+    public function setUsernameCanonical(string $usernameCanonical): self
+    {
+        $this->usernameCanonical = $usernameCanonical;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getEmailCanonical(): ?string
+    {
+        return $this->emailCanonical;
+    }
+
+    public function setEmailCanonical(string $emailCanonical): self
+    {
+        $this->emailCanonical = $emailCanonical;
+
+        return $this;
+    }
+
+    public function getEnabled(): ?bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(bool $enabled): self
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
     {
         return $this->password;
     }
 
-    public function setPassword($password)
+    public function setPassword(string $password): self
     {
         $this->password = $password;
+
+        return $this;
     }
 
-    public function getRoles()
+    public function getPlainPassword(): ?string
     {
-        return array('ROLE_USER');
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(?string $plainPassword): self
+    {
+        $this->plainPassword = $plainPassword;
+
+        return $this;
+    }
+
+    public function getConfirmationToken(): ?string
+    {
+        return $this->confirmationToken;
+    }
+
+    public function setConfirmationToken(?string $confirmationToken): self
+    {
+        $this->confirmationToken = $confirmationToken;
+
+        return $this;
+    }
+
+    /**
+     * @param $role
+     * @return $this
+     */
+    public function removeRole(?string $role): self
+    {
+        if (false !== $key = array_search(strtoupper($role), $this->roles, true)) {
+            unset($this->roles[$key]);
+            $this->roles = array_values($this->roles);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getRoles(): ?array
+    {
+        $roles = $this->roles;
+
+        // we need to make sure to have at least one role
+        $roles[] = static::ROLE_DEFAULT;
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param null|string $role
+     * @return bool
+     */
+    public function hasRole(?string $role)
+    {
+        return in_array(strtoupper($role), $this->getRoles(), true);
+    }
+
+    /**
+     * @param array|null $roles
+     * @return User
+     */
+    public function setRoles(?array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @param null|string $role
+     * @return User
+     */
+    public function addRole(?string $role): self
+    {
+        $role = strtoupper($role);
+
+        if (!in_array($role, $this->roles, true)) {
+            $this->roles[] = $role;
+        }
+
+        return $this;
+    }
+
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    public function isAccountNonLocked()
+    {
+        return true;
+    }
+
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    public function isEnabled()
+    {
+        return $this->enabled;
     }
 
     public function eraseCredentials()
     {
+        $this->plainPassword = null;
+    }
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        $this->enabled = false;
+        $this->roles = [];
+        $this->apartments = new ArrayCollection();
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return (string) $this->getUsername();
+    }
+
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->usernameCanonical,
+            $this->email,
+            $this->emailCanonical,
+            $this->enabled,
+            $this->password,
+        ));
+    }
+
+    public function unserialize($serialized)
+    {
+        $data = unserialize($serialized);
+
+        list(
+            $this->id,
+            $this->username,
+            $this->usernameCanonical,
+            $this->email,
+            $this->emailCanonical,
+            $this->enabled,
+            $this->password,
+        ) = $data;
     }
 }
